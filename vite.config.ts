180df771e6ea3b4eb7535657c08953a1d6f1f0b6
walmartbaglickerthesecond <walmartbaglickerthesecond@gmail.com>
2025-07-21 23:35:27 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MediaRipper/',  // 👈 This matches your repo name – very important!
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
